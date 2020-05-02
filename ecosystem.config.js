835269185e8e949388f203ea80e823b6d4627c63
sibukixxx <sibukixxx@gmail.com>
2020-05-02 21:03:07 +0900

    pm2 update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    apps : [{
-      name: '150.95.185.82',
-      exec_mode : 'cluster',
-      instances: 0,
-      cwd: './current',
-      script: './node_modules/nuxt-start/bin/nuxt-start.js',
-      log_date_format: 'YYYY-MM-DD HH:mm Z'
-    }]
-  }
+  apps : [{
+    name: 'movie-contents.techvit.me',
+    exec_mode : 'cluster',
+    instances: 0,
+    cwd: '/home/deploy/sample/current',
+    script: './node_modules/nuxt-start/bin/nuxt-start.js',
+    log_date_format: 'YYYY-MM-DD HH:mm Z'
+  }]
+}
